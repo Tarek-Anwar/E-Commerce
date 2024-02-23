0c5ea223c54409c8c5ef7f1f13709029a65da00f
Tarek <tarek.anwar.1120@gmail.com>
2024-02-24 00:52:06 +0200

    initial commit

---
+++ app/src/main/java/com/egycode/e_commerce/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.egycode.e_commerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
